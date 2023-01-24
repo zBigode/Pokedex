39535a104db711875c2cc77d2c406f0df3bd9ef2
zBigode <marcos07gabriel@gmail.com>
2023-01-24 11:25:31 -0300

    estilizacao parcial card pokemon

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -1,12 +1,40 @@
 import React from "react";
+import "./pokemon.css";
 
-const Pokemon = (props) =>{
-    const {pokemon} = props
-    console.log(pokemon)
-    return(
-        <div>
-            {pokemon.name}
+const Pokemon = (props) => {
+  const { pokemon } = props;
+  const onHeartClick = () => {
+    console.log("pode favoritar");
+  };
+
+  const heart = "❤";
+
+  return (
+    <div className="pokemon-card">
+      <div className="pokemon-image-container">
+        <img alt={pokemon.name} src={pokemon.sprites.front_default} />
+      </div>
+      <div className="card-body">
+        <div className="card-top">
+          <h3>{pokemon.name}</h3>
+          <div>{pokemon.id}</div>
         </div>
-    )
-}
-export default Pokemon
+        <div className="card-botton">
+          <div className="pokemon-type">
+            {pokemon.types.map((type, index) => {
+              return (
+                <div key={index} className="pokemon-type-text">
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+          <button className="pokemon-heart-btn" onClick={onHeartClick}>
+            {heart}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Pokemon;
