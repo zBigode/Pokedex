237418f4f4b9fc12093031e97e7479125310461e
zBigode <marcos07gabriel@gmail.com>
2023-01-25 16:59:29 -0300

    add tailwind

--- src/components/Pokemon/index.js
+++ src/components/Pokemon/index.js
@@ -6,32 +6,35 @@ const Pokemon = (props) => {
   const onHeartClick = () => {
     console.log("pode favoritar");
   };
-
   const heart = "❤";
 
   return (
-    <div className="pokemon-card">
-      <div className="pokemon-image-container">
-        <img alt={pokemon.name} src={pokemon.sprites.front_default} />
-      </div>
-      <div className="card-body">
-        <div className="card-top">
-          <h3>{pokemon.name}</h3>
-          <div>{pokemon.id}</div>
+    <div>
+      <div className="pokemon-card">
+        <div className="pokemon-image-container">
+          <img alt={pokemon.name} src={pokemon.sprites.front_default} />
         </div>
-        <div className="card-botton">
-          <div className="pokemon-type">
-            {pokemon.types.map((type, index) => {
-              return (
-                <div key={index} className="pokemon-type-text">
-                  {type.type.name}
-                </div>
-              );
-            })}
+        <div className="card-body">
+          <div className="card-top">
+            <h3>{pokemon.name}</h3>
+            <div>{pokemon.id}</div>
           </div>
-          <button className="pokemon-heart-btn" onClick={onHeartClick}>
-            {heart}
-          </button>
+
+          <div className="card-botton">
+            <div className="pokemon-type">
+              {pokemon.types.map((type, index) => {
+                return (
+                  <div key={index} className="pokemon-type-text">
+                    {type.type.name}
+                    
+                  </div>
+                );
+              })}
+            </div>
+            <button className="pokemon-heart-btn" onClick={onHeartClick}>
+              {heart}
+            </button>
+          </div>
         </div>
       </div>
     </div>
